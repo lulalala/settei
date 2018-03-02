ba76434fc2f02484605ea40837e9cfd6e460b889
lulalala <mark@goodlife.tw>
2018-03-03 00:49:15 +0800

    Template for basic setup

--- templates/setting.rb
+++ templates/setting.rb
@@ -1,1 +1,11 @@
-require 'settei'
+require 'settei/serializers/simple_serializer'
+require 'settei/accessors/simple_accessor'
+require 'settei/accessors/host_plugin'
+
+serializer = Settei::Serializers::SimpleSerializer.new(
+  file_dir_path: "#{File.dirname(__FILE__)}/environments"
+)
+Setting = Settei::Accessors::SimpleAccessor.new(
+  config: serializer.load
+)
+Setting.extend Settei::Accessor::HostPlugin
