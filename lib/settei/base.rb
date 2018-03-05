ceaed4fe783abc2e6bb8660ef833636a861c07dc
lulalala <mark@goodlife.tw>
2018-03-05 11:02:14 +0800

    Add more methods for accessing configuration

--- lib/settei/base.rb
+++ lib/settei/base.rb
@@ -2,11 +2,25 @@
 module Settei
   class Base
     extend Forwardable
-    def_delegator :@config, :dig
+    def_delegators :@config, :dig, :merge!, :fetch, :has_key?, :include?, :key?, :member?, :to_hash, :to_h
 
     # @params config [Hash] configuration
     def initialize(config)
       self.config = config
+    end
+
+    def dig_and_wrap(*args)
+      result = dig(*args)
+      if result.is_a?(Hash)
+        self.class.new(result)
+      else
+        result
+      end
+    end
+
+    def merge(*args)
+      new_hash = @config.merge(*args)
+      self.class.new(new_hash)
     end
 
     private
---
+++ spec/base_spec.rb
@@ -0,0 +1,58 @@
+require 'settei/base'
+
+RSpec.describe Settei::Base do
+  subject {
+    described_class.new(
+      foo: {
+        bar: {
+          baz: 42
+        }
+      }
+    )
+  }
+
+  describe '#dig' do
+    it 'returns dig value as is for the inner hash' do
+      result = subject.dig(:foo, :bar)
+      expect(result).to be_a Hash
+      expect(result).to eq({'baz' => 42})
+    end
+
+    it 'returns dig value as is if it is not a hash' do
+      result = subject.dig(:foo, :bar, :baz)
+      expect(result).to eq(42)
+    end
+  end
+
+  describe '#dig_and_wrap' do
+    it 'returns a Settei::Base for the inner hash' do
+      result = subject.dig_and_wrap(:foo, :bar)
+      expect(result).to be_a described_class
+      expect(result.to_h).to eq({'baz' => 42})
+      expect(result.to_hash).to eq({'baz' => 42})
+    end
+
+    it 'returns dig value as is if it is not a hash' do
+      result = subject.dig_and_wrap(:foo, :bar, :baz)
+      expect(result).to eq(42)
+    end
+  end
+
+  describe '#merge!' do
+    it 'merge hash' do
+      subject.merge!(foo: 2, bar: 3)
+      expect(subject.fetch(:foo)).to eq(2)
+      expect(subject.fetch(:bar)).to eq(3)
+    end
+  end
+
+  describe '#merge' do
+    it 'merge hash' do
+      new_hash = {"foo" => 2, "bar" => 3}
+      previous_hash = subject.to_h
+      result = subject.merge(new_hash)
+      expect(subject.to_h).to eq(previous_hash)
+      expect(result.to_h).to eq(new_hash)
+    end
+  end
+end
