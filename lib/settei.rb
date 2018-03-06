b4ee7813b931c01a70b0479fae3346ca55512a84
lulalala <mark@goodlife.tw>
2018-03-06 22:54:09 +0800

    refactor

--- lib/settei.rb
+++ lib/settei.rb
@@ -1,4 +1,4 @@
 require_relative "settei/version"
 require_relative "settei/base"
 require_relative "settei/loaders/simple_loader"
-require_relative 'settei/railtie' if !!defined?(Rails)
+require_relative 'settei/railtie' if defined?(Rails)
